9c2a570a5c0e6c3556acd14b4138e05e4d29e2ef
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-06-26 11:23:27 +0800

    Initial commit with GenerateContent and GenerateContentStream

---
+++ cmd/hello/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	ctx := context.Background()
+	// Access your API key as an environment variable (see "Set up your API key" above)
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("API_KEY")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	// The Gemini 1.5 models are versatile and work with most use cases
+	model := client.GenerativeModel("gemini-1.5-flash")
+
+	resp, err := model.GenerateContent(ctx, genai.Text("What is the significance of the number 42?"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	printResponse(resp)
+}
+
+func printResponse(resp *genai.GenerateContentResponse) {
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				fmt.Println(part)
+			}
+		}
+	}
+	fmt.Println("---")
+}
---
+++ cmd/helloStream/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	ctx := context.Background()
+	// Access your API key as an environment variable (see "Set up your API key" above)
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("API_KEY")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	// The Gemini 1.5 models are versatile and work with most use cases
+	model := client.GenerativeModel("gemini-1.5-flash")
+
+	iter := model.GenerateContentStream(ctx, genai.Text("What is the significance of the number 42?"))
+
+	for {
+		resp, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		printResponse(resp)
+	}
+}
+
+func printResponse(resp *genai.GenerateContentResponse) {
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				fmt.Println(part)
+			}
+		}
+	}
+	fmt.Println("---")
+}
