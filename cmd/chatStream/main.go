59ade5b3b1bf695d5a5c6cfe74728782a254b778
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-06-26 12:46:01 +0800

    feat: add chat streaming response

---
+++ cmd/chatStream/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	ctx := context.Background()
+	// Access your API key as an environment variable (see "Set up your API key" above)
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("API_KEY")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	// The Gemini 1.5 models are versatile and work with multi-turn conversations (like chat)
+	model := client.GenerativeModel("gemini-1.5-flash")
+	// Initialize the chat
+	cs := model.StartChat()
+	cs.History = []*genai.Content{
+		&genai.Content{
+			Parts: []genai.Part{
+				genai.Text("Hello, I have 2 dogs in my house."),
+			},
+			Role: "user",
+		},
+		&genai.Content{
+			Parts: []genai.Part{
+				genai.Text("Great to meet you. What would you like to know?"),
+			},
+			Role: "model",
+		},
+	}
+
+	iter := cs.SendMessageStream(ctx, genai.Text("How many paws are in my house?"))
+	for {
+		resp, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		printResponse(resp)
+	}
+
+}
+
+func printResponse(resp *genai.GenerateContentResponse) {
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				fmt.Println(part)
+			}
+		}
+	}
+	fmt.Println("---")
+}
