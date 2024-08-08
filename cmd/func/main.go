5ab6506b6e34a3f971ea7bdadd4caf1ceee8ee74
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-08-08 17:10:35 +0800

    feat: flesh out function calling example

--- cmd/func/main.go
+++ cmd/func/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/google/generative-ai-go/genai"
 	"github.com/siuyin/aigotut/client"
+	"github.com/siuyin/aigotut/gfmt"
 )
 
 var lightControlTool = &genai.Tool{
@@ -17,7 +18,7 @@ var lightControlTool = &genai.Tool{
 			Type: genai.TypeObject,
 			Properties: map[string]*genai.Schema{
 				"brightness": {
-					Type:        genai.TypeString,
+					Type:        genai.TypeInteger,
 					Description: "Light level from 0 to 100. Zero is off and 100 is full brightness.",
 				},
 				"colorTemp": {
@@ -38,30 +39,48 @@ func main() {
 
 	// Start chat session.
 	cs := cl.Model.StartChat()
-	prompt := "Dim the light so that the room feels cosy and warm."
-	//prompt := "Make the room as bright as the day for reading."
-	//prompt := "Simulate a cool cloudy day, the light should not be too bright."
-	//prompt := "Light the room with a warm, very dim, night light."
-	resp, err := cs.SendMessage(context.Background(), genai.Text(prompt))
-	if err != nil {
-		log.Fatal(err)
+	prompts := []string{
+		"Dim the light so that the room feels cosy and warm. Let me know when it is done",
+		"Make the room as bright as the day for reading.",
+		"Simulate a cool cloudy day, the light should not be too bright.",
+		"Light the room with a warm, very dim, night light.",
 	}
+	for _, p := range prompts {
+		fmt.Printf("prompt: %s\n", p)
+		resp, err := cs.SendMessage(context.Background(), genai.Text(p))
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// check response include function call
-	part := resp.Candidates[0].Content.Parts[0]
-	funcall, ok := part.(genai.FunctionCall)
-	if !ok {
-		log.Fatalf("Expected type FunctionCall, got %T", part)
+		fmt.Printf("number of candidates in response: %d\n", len(resp.Candidates))
+		// check response include function call
+		part := resp.Candidates[0].Content.Parts[0]
+		funcall, ok := part.(genai.FunctionCall)
+		if !ok {
+			log.Printf("Expected type FunctionCall, got %T", part)
+			continue
+		}
+
+		if funcall.Name != "setLightValues" {
+			log.Printf("ERROR: received bad function call: %s", funcall.Name)
+			continue
+		}
+
+		brightness, colorTemp:=setLightValues(funcall.Args["brightness"].(float64), funcall.Args["colorTemp"].(string))
+		resp,err=cs.SendMessage(context.Background(),genai.FunctionResponse{
+			Name: funcall.Name,
+			Response: map[string]any{"brightness":brightness,"colorTemp":colorTemp},
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		gfmt.PrintResponse(resp)
 	}
-	if g, e := funcall.Name, lightControlTool.FunctionDeclarations[0].Name; g != e {
-		log.Fatalf("Expected FunctionCall.Name %q, got %q", e, g)
-	}
-	fmt.Printf("Received function call response:\n%q\n\n", part)
 
 }
 
-func setLightValues(brightness int, colorTemp string) map[string]any {
-	return map[string]any{
-		"brightness": brightness,
-		"colorTemp":  colorTemp}
+func setLightValues(brightness float64, colorTemp string) (float64, string) {
+	fmt.Printf("Setting room brightness to %g at color temperature %q\n---\n", brightness, colorTemp)
+	return brightness, colorTemp
 }
